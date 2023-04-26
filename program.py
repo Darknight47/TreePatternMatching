b1f245ce544ca945fc34c5f523a2cc55dcccb9d0
DarkNight <farhadgunners@gmail.com>
2023-04-26 14:12:11 +0200

    updated the program.py

--- program.py
+++ program.py
@@ -20,14 +20,17 @@ def print_tree(node, level=0):
     if node.rightChildElement is not None:
         print_tree(node.rightChildElement, level + 1)
 
-def readingFromFile(lineTemp):
+def readingFromFile(lineTemp, bs= False):
     lineTemp = file.readline()
     lineTemp = lineTemp.strip()
     lineTemp = lineTemp.replace(" ", "")
-    tempArr = re.split(r'[(), ]+', lineTemp)
+    if(bs):
+        tempArr = tempArr = re.findall(r'\w+|\\=|\S', lineTemp)
+    else:
+        tempArr = re.split(r'[(), ]+', lineTemp)
     return tempArr
 
-def mother_father_tree(lineTemp, tempArr):
+def tree_builder(lineTemp, tempArr, bs = False, nephew = False):
     treeRoot = Node(tempArr[0])
     leftChild = Node(tempArr[1], parent=treeRoot)
     rightChild = Node(tempArr[2], parent=treeRoot)
@@ -54,7 +57,42 @@ def mother_father_tree(lineTemp, tempArr):
     leftChild = Node(tempArr[1], parent=tempRoot)
     tempRoot.addLeftChild(leftChild)
     nodes.append(tempRoot)
-    nodes.append(leftChild)        
+    nodes.append(leftChild)
+    
+    if(bs):
+        tempArr = readingFromFile(lineTemp=lineTemp, bs=True)
+        tempRoot = Node(tempArr[1], parent=leftChild)
+        leftChild.addLeftChild(tempRoot)
+        leftChild = Node(tempArr[0], parent=tempRoot)
+        rightChild = Node(tempArr[2], parent=tempRoot)
+        tempRoot.addLeftChild(leftChild)
+        tempRoot.addRightChild(rightChild)    
+        nodes.append(tempRoot)
+        nodes.append(leftChild)
+        nodes.append(rightChild)
+    
+    if(nephew):
+        #rightChild = treeRoot.rightChildElement
+        tempArr = readingFromFile(lineTemp=lineTemp)    
+        tempRoot = Node(tempArr[0], parent=treeRoot.rightChildElement)
+        #leftChild.addLeftChild(tempRoot)
+        treeRoot.rightChildElement.addRightChild(tempRoot)
+        leftChild = Node(tempArr[1], parent=tempRoot)
+        rightChild = Node(tempArr[2], parent=tempRoot)
+        tempRoot.addLeftChild(leftChild)
+        tempRoot.addRightChild(rightChild)    
+        nodes.append(tempRoot)
+        nodes.append(leftChild)
+        nodes.append(rightChild)
+    
+        tempArr = readingFromFile(lineTemp=lineTemp)
+        tempRoot = Node(tempArr[0], parent=leftChild)
+        leftChild.addLeftChild(tempRoot)
+        leftChild = Node(tempArr[1], parent=tempRoot)
+        tempRoot.addLeftChild(leftChild)
+        nodes.append(tempRoot)
+        nodes.append(leftChild)
+        
     return treeRoot 
 
 
@@ -66,12 +104,16 @@ with open("text.txt") as file:
             if(indx == 0):
                 diction["MotherTree"] = treeRoot
                 indx += 1
-            #print_tree(treeRoot)
-        if not lineTemp: #End of the File.
-            if(indx == 1):
+            elif(indx == 1):
                 diction["FatherTree"] = treeRoot
                 indx += 1
-            #print_tree(treeRoot)
+            elif(indx == 2):
+                diction["BrotherTree"] = treeRoot
+                indx += 1
+        if not lineTemp: #End of the File.
+            if(indx == 3):
+                diction["SisterTree"] = treeRoot
+                indx += 1
             break
         lineTemp = lineTemp.strip()
         lineTemp = lineTemp.replace(" ", "")
@@ -80,6 +122,16 @@ with open("text.txt") as file:
             tempFact = Fact(tempArr[0], tempArr[1], tempArr[2])
             facts.append(tempFact)
         elif(tempArr[len(tempArr) - 1] == ':-'):
-            treeRoot = mother_father_tree(lineTemp=lineTemp, tempArr=tempArr)
-            print_tree(treeRoot)
+            if(indx == 0):
+                treeRoot = tree_builder(lineTemp=lineTemp, tempArr=tempArr, nephew=True)
+                print_tree(treeRoot)
+            elif(indx == 1):
+                treeRoot = tree_builder(lineTemp=lineTemp, tempArr=tempArr)
+                print_tree(treeRoot)
+            elif(indx == 2):
+                treeRoot = tree_builder(lineTemp=lineTemp, tempArr=tempArr, bs=True)
+                print_tree(treeRoot)
+            elif(indx == 3):
+                treeRoot = tree_builder(lineTemp=lineTemp, tempArr=tempArr, bs=True)
+                print_tree(treeRoot)
 print(2)
